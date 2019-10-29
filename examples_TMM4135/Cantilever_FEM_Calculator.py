99d5ae2ab8e7a4db77099a9979e86767dd994235
opetlund <opetl@hotmail.com>
2019-10-29 14:06:37 +0100

    Added cfc.plante(..) which is the same as tri.plante(..) / tri.tri3e(..)

--- examples_TMM4135/Cantilever_FEM_Calculator.py
+++ examples_TMM4135/Cantilever_FEM_Calculator.py
@@ -14,17 +14,17 @@ import calfem.vis as cfv
 
 numElementNodes = 3  # Valid numbers 3, 4, 6, 9
 
-elTypeInfo= [-1,'Unknown elementtype']
+elTypeInfo = [-1, 'Unknown elementtype']
 
 meshText = 'Unknown mesh'
 if numElementNodes == 3:
-    elTypeInfo= [2,'3 Node Triangle']
+    elTypeInfo = [2, '3 Node Triangle']
 elif numElementNodes == 4:
-    elTypeInfo= [3,'4 node Quad mesh']
+    elTypeInfo = [3, '4 node Quad mesh']
 elif numElementNodes == 6:
-    elTypeInfo= [9,'6 node Triangle mesh']
+    elTypeInfo = [9, '6 node Triangle mesh']
 elif numElementNodes == 9:
-    elTypeInfo= [10,'9 node Quad mesh']
+    elTypeInfo = [10, '9 node Quad mesh']
 
 # Number of nodes: Should be odd numbers in order to handle
 
@@ -44,41 +44,41 @@ if numElementNodes == 6 or numElementNodes == 9:
     numElementsX = (numNodesX-1) // 2
     numElementsY = (numNodesY-1) // 2
 else:
-    numElementsX = numNodesX -1
-    numElementsY = numNodesY -1
+    numElementsX = numNodesX - 1
+    numElementsY = numNodesY - 1
 
 bDrawMesh = True
 
 # Cantilever with dimensions H x L x thickness
-H         =  2.0
-L         = 10.0
-thickness =  0.1
+H = 2.0
+L = 10.0
+thickness = 0.1
 
 # Distributed load in x and y, load pr unit area
-eq = np.array([0.,1.0e3])
-eq = np.array([0.,0.])
-#End load, Given as resultant
+eq = np.array([0., 1.0e3])
+eq = np.array([0., 0.])
+# End load, Given as resultant
 
-endLoadXY = np.array([0.0,3.0e6])
+endLoadXY = np.array([0.0, 3.0e6])
 #endLoadXY = np.array([3.0e6,0])
-#endLoadXY = np.array([4.2e9,0.0]) # Should give unit disp at Poisson = 0
+# endLoadXY = np.array([4.2e9,0.0]) # Should give unit disp at Poisson = 0
 
 
-eqTotal = eq * L * H * thickness #Total load for plotting purpose
+eqTotal = eq * L * H * thickness  # Total load for plotting purpose
 
 # Material properties and thickness
 
-ep = [1,thickness]
-E  = 2.1e11
+ep = [1, thickness]
+E = 2.1e11
 nu = 0.3
 Dmat = np.mat([
-        [ 1.0,  nu,  0.],
-        [  nu, 1.0,  0.],
-        [  0.,  0., (1.0-nu)/2.0]]) * E/(1.0-nu**2)
+    [1.0,  nu,  0.],
+    [nu, 1.0,  0.],
+    [0.,  0., (1.0-nu)/2.0]]) * E/(1.0-nu**2)
 
-numNodes    = numNodesX * numNodesY
+numNodes = numNodesX * numNodesY
 numElements = numElementsX * numElementsY
-if numElementNodes in [3,6]:
+if numElementNodes in [3, 6]:
     numElements *= 2
 
 L_elx = L / (numNodesX-1)
@@ -86,29 +86,30 @@ L_ely = H / (numNodesY-1)
 
 nelnod = 6
 
-coords = np.zeros((numNodes,2))
-dofs   = np.zeros((numNodes,2),int)       #Dofs is starting on 1 on first dof
-edofs  = np.zeros((numElements,numElementNodes*2),int) #edofs are also starting on 1 based dof
+coords = np.zeros((numNodes, 2))
+dofs = np.zeros((numNodes, 2), int)  # Dofs is starting on 1 on first dof
+# edofs are also starting on 1 based dof
+edofs = np.zeros((numElements, numElementNodes*2), int)
 
 
-inod = 0 # The coords table starts numbering on 0
-idof = 1 # The values of the dofs start on 1
+inod = 0  # The coords table starts numbering on 0
+idof = 1  # The values of the dofs start on 1
 ndofs = numNodes * 2
 
 # Set the node coordinates and node dofs
 
 for i in range(numNodesX):
     for j in range(numNodesY):
-        coords[inod,0] = L_elx * i
-        coords[inod,1] = L_ely * j
-        dofs[inod,0] = idof
-        dofs[inod,1] = idof+1
+        coords[inod, 0] = L_elx * i
+        coords[inod, 1] = L_ely * j
+        dofs[inod, 0] = idof
+        dofs[inod, 1] = idof+1
         idof += 2
         inod += 1
 
 # Set the element connectivites and element dofs
-elnods = np.zeros((numElements,numElementNodes),int)
-eldofs = np.zeros((numElements,numElementNodes*2),int)
+elnods = np.zeros((numElements, numElementNodes), int)
+eldofs = np.zeros((numElements, numElementNodes*2), int)
 
 iel = 0
 for ip in range(numPatchX):
@@ -117,43 +118,48 @@ for ip in range(numPatchX):
         jj = jp*2
         # 0 based node numbers, 9 nodes of a 3x3 patch
         nod9 = np.array([
-            (ii  )*numNodesY + (jj  ),
-            (ii+1)*numNodesY + (jj  ),
-            (ii+2)*numNodesY + (jj  ),
-            (ii  )*numNodesY + (jj+1),
+            (ii)*numNodesY + (jj),
+            (ii+1)*numNodesY + (jj),
+            (ii+2)*numNodesY + (jj),
+            (ii)*numNodesY + (jj+1),
             (ii+1)*numNodesY + (jj+1),
             (ii+2)*numNodesY + (jj+1),
-            (ii  )*numNodesY + (jj+2),
+            (ii)*numNodesY + (jj+2),
             (ii+1)*numNodesY + (jj+2),
-            (ii+2)*numNodesY + (jj+2)],'i')
+            (ii+2)*numNodesY + (jj+2)], 'i')
 
         if numElementNodes == 3:
             for i in range(2):
                 for j in range(2):
-                    elnods[iel,:] = [nod9[3*i+j],nod9[3*i+j+1],nod9[3*(i+1)+j+1]]
+                    elnods[iel, :] = [nod9[3*i+j],
+                                      nod9[3*i+j+1], nod9[3*(i+1)+j+1]]
                     iel += 1
-                    elnods[iel,:] = [nod9[3*(i+1)+j+1],nod9[3*(i+1)+j],nod9[3*i+j]]
+                    elnods[iel, :] = [
+                        nod9[3*(i+1)+j+1], nod9[3*(i+1)+j], nod9[3*i+j]]
                     iel += 1
         elif numElementNodes == 6:
-            elnods[iel,:] = [nod9[0],nod9[2],nod9[8],nod9[1],nod9[5],nod9[4]]
+            elnods[iel, :] = [nod9[0], nod9[2],
+                              nod9[8], nod9[1], nod9[5], nod9[4]]
             iel += 1
-            elnods[iel,:] = [nod9[8],nod9[6],nod9[0],nod9[7],nod9[3],nod9[4]]
+            elnods[iel, :] = [nod9[8], nod9[6],
+                              nod9[0], nod9[7], nod9[3], nod9[4]]
             iel += 1
         elif numElementNodes == 4:
             for i in range(2):
                 for j in range(2):
-                    elnods[iel,:] = [nod9[3*i+j],nod9[3*i+j+1],nod9[3*(i+1)+j+1],nod9[3*(i+1)+j]]
+                    elnods[iel, :] = [nod9[3*i+j], nod9[3*i+j+1],
+                                      nod9[3*(i+1)+j+1], nod9[3*(i+1)+j]]
                     iel += 1
         elif numElementNodes == 9:
-            elnods[iel,:] = [nod9[0],nod9[2],nod9[8],nod9[6],
-                             nod9[1],nod9[5],nod9[7],nod9[3],
-                             nod9[4]]
+            elnods[iel, :] = [nod9[0], nod9[2], nod9[8], nod9[6],
+                              nod9[1], nod9[5], nod9[7], nod9[3],
+                              nod9[4]]
             iel += 1
 
 
 for iel in range(elnods.shape[0]):
-    eldofs[iel, ::2] = elnods[iel,:] * 2 + 1 # The x dofs
-    eldofs[iel,1::2] = elnods[iel,:] * 2 + 2 # The y dofs
+    eldofs[iel, ::2] = elnods[iel, :] * 2 + 1  # The x dofs
+    eldofs[iel, 1::2] = elnods[iel, :] * 2 + 2  # The y dofs
 
 
 # Draw the mesh.
@@ -168,72 +174,84 @@ if bDrawMesh:
     cfv.showAndWait()
 
 # Extract element coordinates
-ex, ey = cfc.coordxtr(eldofs,coords,dofs)
+ex, ey = cfc.coordxtr(eldofs, coords, dofs)
 
 # Set fixed boundary condition on left side, i.e. nodes 0-nNody
-bc = np.array(np.zeros(numNodesY*2),'i')
+bc = np.array(np.zeros(numNodesY*2), 'i')
 idof = 1
 for i in range(numNodesY):
     idx = i*2
-    bc[idx]   = idof
+    bc[idx] = idof
     bc[idx+1] = idof+1
     idof += 2
 
 # Assemble stiffness matrix
 
-K = np.zeros((ndofs,ndofs))
-R = np.zeros((ndofs,1))
+K = np.zeros((ndofs, ndofs))
+R = np.zeros((ndofs, 1))
 
-#Set the load at the right hand edge
+# Set the load at the right hand edge
 for i in range(numNodesY):
-    R[-(i*2+2),0] = endLoadXY[0] / numNodesY
-    R[-(i*2+1),0] = endLoadXY[1] / numNodesY
+    R[-(i*2+2), 0] = endLoadXY[0] / numNodesY
+    R[-(i*2+1), 0] = endLoadXY[1] / numNodesY
+
+# Choose which solution to use
+ourSolution = True
 
 for iel in range(numElements):
-    if numElementNodes == 3:
-        K_el, f_el = tri.tri3e(ex[iel],ey[iel],Dmat,thickness,eq)
-    elif numElementNodes == 6:
-        K_el, f_el = tri.tri6e(ex[iel],ey[iel],Dmat,thickness,eq)
-    elif numElementNodes == 4:
-        K_el, f_el = quad.quad4e(ex[iel],ey[iel],Dmat,thickness,eq)
-    elif numElementNodes == 9:
-        K_el, f_el = quad.quad9e(ex[iel],ey[iel],Dmat,thickness,eq)
+    if ourSolution:
+        if numElementNodes == 3:
+            K_el, f_el = tri.tri3e(ex[iel], ey[iel], Dmat, thickness, eq)
+        elif numElementNodes == 6:
+            K_el, f_el = tri.tri6e(ex[iel], ey[iel], Dmat, thickness, eq)
+        elif numElementNodes == 4:
+            K_el, f_el = quad.quad4e(ex[iel], ey[iel], Dmat, thickness, eq)
+        elif numElementNodes == 9:
+            K_el, f_el = quad.quad9e(ex[iel], ey[iel], Dmat, thickness, eq)
+    else:
+        if numElementNodes == 3:
+            # Tror dette tilsvarer tri.tri3e(..), men den fungerer ikke helt
+            # Eller samme som tri.plante(..)
+            K_el, f_el = cfc.plante(ex[iel], ey[iel], ep, Dmat, eq)
 
-    cfc.assem(eldofs[iel],K,K_el,R,f_el)
+    cfc.assem(eldofs[iel], K, K_el, R, f_el)
 
-r, R0 = cfc.solveq(K,R,bc)
+r, R0 = cfc.solveq(K, R, bc)
 
-nodMiddle = numNodesY//2 +1  # Mid nod on right edge
-xC = r[-(nodMiddle*2)  ,0] # 2 dofs per node, so this is the middle dof on end
-yC = r[-(nodMiddle*2)+1,0] # 2 dofs per node, so this is the middle dof on end
-print("Displacement center node right end,  x:{:12.3e}   y:{:12.3e}".format(xC, yC))
+nodMiddle = numNodesY//2 + 1  # Mid nod on right edge
+xC = r[-(nodMiddle*2), 0]  # 2 dofs per node, so this is the middle dof on end
+# 2 dofs per node, so this is the middle dof on end
+yC = r[-(nodMiddle*2)+1, 0]
+print(
+    "Displacement center node right end,  x:{:12.3e}   y:{:12.3e}".format(xC, yC))
 
 # Sum uf reaction forces
-R0Sum = np.zeros(2,'f')
-for i in range(0,(numNodesY*2),2):
-    R0Sum[0] += R0[i  ,0]
-    R0Sum[1] += R0[i+1,0]
+R0Sum = np.zeros(2, 'f')
+for i in range(0, (numNodesY*2), 2):
+    R0Sum[0] += R0[i, 0]
+    R0Sum[1] += R0[i+1, 0]
 
-eqTotal = eq * L * H * thickness #Total load for plotting purpose
-print("Total reaction force in x:{:12.3e} y:{:12.3e})".format(R0Sum[0],R0Sum[1]))
+eqTotal = eq * L * H * thickness  # Total load for plotting purpose
+print("Total reaction force in x:{:12.3e} y:{:12.3e})".format(
+    R0Sum[0], R0Sum[1]))
 
 # Draw the displacements
 
 
 if bDrawMesh:
-    disp = np.array(np.zeros((numNodes,2)),'f')
-    rMax = max(abs(max(r)),abs(min(r)))
+    disp = np.array(np.zeros((numNodes, 2)), 'f')
+    rMax = max(abs(max(r)), abs(min(r)))
     scale = 0.15 * L / rMax
 
-    for i in range( np.size(disp,0)):
-        disp[i,0] = r[i*2   ,0] * scale
-        disp[i,1] = r[i*2 +1,0] * scale
+    for i in range(np.size(disp, 0)):
+        disp[i, 0] = r[i*2, 0] * scale
+        disp[i, 1] = r[i*2 + 1, 0] * scale
 
     cfv.drawDisplacements(displacements=disp,
-        coords=coords,
-        edof=eldofs,
-        dofsPerNode=2,
-        elType=elTypeInfo[0],
-        title=elTypeInfo[1])
+                          coords=coords,
+                          edof=eldofs,
+                          dofsPerNode=2,
+                          elType=elTypeInfo[0],
+                          title=elTypeInfo[1])
 
     cfv.showAndWait()
--- examples_TMM4135/triangles.py
+++ examples_TMM4135/triangles.py
@@ -6,23 +6,24 @@ Created on Sun Oct 21 08:15:51 2018
 """
 import numpy as np
 
-def plante(ex,ey,ep,D,eq=None):
-    
+
+def plante(ex, ey, ep, D, eq=None):
+
     Dshape = D.shape
     if Dshape[0] != 3:
         raise NameError('Wrong constitutive dimension in plante')
-        
-    if ep[0] == 1 :
-        return tri3e(ex,ey,D,ep[1],eq)
+
+    if ep[0] == 1:
+        return tri3e(ex, ey, D, ep[1], eq)
     else:
         Dinv = np.inv(D)
-        return tri3e(ex,ey,Dinv,ep[1],eq)
+        return tri3e(ex, ey, Dinv, ep[1], eq)
 
 
-def tri3e(ex,ey,D,th,eq=None):
+def tri3e(ex, ey, D, th, eq=None):
     """
     Compute the stiffness matrix for a two dimensional beam element.
-    
+
     :param list ex: element x coordinates [x1, x2, x3]
     :param list ey: element y coordinates [y1, y2, y3]
     :param list D : 2D constitutive matrix
@@ -31,47 +32,51 @@ def tri3e(ex,ey,D,th,eq=None):
     :return mat Ke: element stiffness matrix [6 x 6]
     :return mat fe: consistent load vector [6 x 1] (if eq!=None)
     """
-    
-    tmp = np.matrix([[1,ex[0],ey[0]],
-                     [1,ex[1],ey[1]],
-                     [1,ex[2],ey[2]]])
-    
+
+    tmp = np.matrix([[1, ex[0], ey[0]],
+                     [1, ex[1], ey[1]],
+                     [1, ex[2], ey[2]]])
+
     A2 = np.linalg.det(tmp)  # Double of triangle area
-    A  = A2 / 2.0
-       
-    cyclic_ijk = [0,1,2,0,1]      # Cyclic permutation of the nodes i,j,k
+    A = A2 / 2.0
+
+    cyclic_ijk = [0, 1, 2, 0, 1]      # Cyclic permutation of the nodes i,j,k
 
     # TODO: fill out missing parts (or reformulate completely)
+    # TODO: Compute fe
     zeta_px, zeta_py = zeta_partials_x_and_y(ex, ey)
 
-    B = np.mat(np.zeros((3,6)))
+    B = np.mat(np.zeros((3, 6)))
+    # Set columns of B
     for i in range(3):
-        B[:,i * 2] = np.array([[zeta_py[i]], [0], [zeta_px[i]]])
-        B[:,i * 2 + 1] = np.array([[0], [zeta_px[i]], [zeta_py[i]]])
-    
+        B[:, i * 2] = np.array([[zeta_py[i]], [0], [zeta_px[i]]])
+        B[:, i * 2 + 1] = np.array([[0], [zeta_px[i]], [zeta_py[i]]])
+
+    # Compute element stifness matrix
     Ke = A * th * B.T * D * B
     if eq is None:
         return Ke
     else:
-        fe = np.mat(np.zeros((6,1)))
+        fe = np.mat(np.zeros((6, 1)))
         return Ke, fe
-    
-def zeta_partials_x_and_y(ex,ey):
+
+
+def zeta_partials_x_and_y(ex, ey):
     """
     Compute partials of area coordinates with respect to x and y.
-    
+
     :param list ex: element x coordinates [x1, x2, x3]
     :param list ey: element y coordinates [y1, y2, y3]
     """
-    
-    tmp = np.matrix([[1,ex[0],ey[0]],
-                     [1,ex[1],ey[1]],
-                     [1,ex[2],ey[2]]])
-    
+
+    tmp = np.matrix([[1, ex[0], ey[0]],
+                     [1, ex[1], ey[1]],
+                     [1, ex[2], ey[2]]])
+
     A2 = np.linalg.det(tmp)  # Double of triangle area
-       
-    cyclic_ijk = [0,1,2,0,1]      # Cyclic permutation of the nodes i,j,k
-    
+
+    cyclic_ijk = [0, 1, 2, 0, 1]      # Cyclic permutation of the nodes i,j,k
+
     zeta_px = np.zeros(3)           # Partial derivative with respect to x
     zeta_py = np.zeros(3)           # Partial derivative with respect to y
 
@@ -79,26 +84,27 @@ def zeta_partials_x_and_y(ex,ey):
         j = (i + 1) % 3
         k = (i + 2) % 3
         zeta_px[i] = (ey[j] - ey[k]) / A2
-        zeta_py[i] = (ex[k] - ex[j]) / A2 
+        zeta_py[i] = (ex[k] - ex[j]) / A2
 
     return zeta_px, zeta_py
 
 # Functions for 6 node triangle
-    
-def tri6_area(ex,ey):
-        
-    tmp = np.matrix([[1,ex[0],ey[0]],
-                     [1,ex[1],ey[1]],
-                     [1,ex[2],ey[2]]])
-    
+
+
+def tri6_area(ex, ey):
+
+    tmp = np.matrix([[1, ex[0], ey[0]],
+                     [1, ex[1], ey[1]],
+                     [1, ex[2], ey[2]]])
+
     A = np.linalg.det(tmp) / 2
-    
+
     return A
 
 
 def tri6_shape_functions(zeta):
-    
-    cyclic_ijk = [0,1,2,0,1]      # Cyclic permutation of the nodes i,j,k
+
+    cyclic_ijk = [0, 1, 2, 0, 1]      # Cyclic permutation of the nodes i,j,k
 
     N6 = np.zeros(6)
 
@@ -107,58 +113,54 @@ def tri6_shape_functions(zeta):
     return N6
 
 
-def tri6_shape_function_partials_x_and_y(zeta,ex,ey):
-    
-    zeta_px, zeta_py = zeta_partials_x_and_y(ex,ey)
-    
+def tri6_shape_function_partials_x_and_y(zeta, ex, ey):
+
+    zeta_px, zeta_py = zeta_partials_x_and_y(ex, ey)
+
     N6_px = np.zeros(6)
     N6_py = np.zeros(6)
-    
-    cyclic_ijk = [0,1,2,0,1]      # Cyclic permutation of the nodes i,j,k
+
+    cyclic_ijk = [0, 1, 2, 0, 1]      # Cyclic permutation of the nodes i,j,k
 
     # TODO: fill out missing parts (or reformulate completely)
 
     return N6_px, N6_py
 
 
-def tri6_Bmatrix(zeta,ex,ey):
-    
-    nx,ny = tri6_shape_function_partials_x_and_y(zeta, ex, ey)
+def tri6_Bmatrix(zeta, ex, ey):
 
-    Bmatrix = np.matrix(np.zeros((3,12)))
+    nx, ny = tri6_shape_function_partials_x_and_y(zeta, ex, ey)
+
+    Bmatrix = np.matrix(np.zeros((3, 12)))
 
     # TODO: fill out missing parts (or reformulate completely)
 
     return Bmatrix
 
 
-def tri6_Kmatrix(ex,ey,D,th,eq=None):
-    
-    zetaInt = np.array([[0.5,0.5,0.0],
-                        [0.0,0.5,0.5],
-                        [0.5,0.0,0.5]])
-    
-    wInt = np.array([1.0/3.0,1.0/3.0,1.0/3.0])
+def tri6_Kmatrix(ex, ey, D, th, eq=None):
 
-    A    = tri6_area(ex,ey)
-    
-    Ke = np.matrix(np.zeros((12,12)))
+    zetaInt = np.array([[0.5, 0.5, 0.0],
+                        [0.0, 0.5, 0.5],
+                        [0.5, 0.0, 0.5]])
+
+    wInt = np.array([1.0/3.0, 1.0/3.0, 1.0/3.0])
+
+    A = tri6_area(ex, ey)
+
+    Ke = np.matrix(np.zeros((12, 12)))
 
     # TODO: fill out missing parts (or reformulate completely)
 
     if eq is None:
         return Ke
     else:
-        fe = np.matrix(np.zeros((12,1)))
+        fe = np.matrix(np.zeros((12, 1)))
 
         # TODO: fill out missing parts (or reformulate completely)
 
         return Ke, fe
 
-def tri6e(ex,ey,D,th,eq=None):
-    return tri6_Kmatrix(ex,ey,D,th,eq)
 
-
-
-
-  
+def tri6e(ex, ey, D, th, eq=None):
+    return tri6_Kmatrix(ex, ey, D, th, eq)
