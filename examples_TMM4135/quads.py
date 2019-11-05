f1a4bbf7a39ce28dde0161444d5c6a7bb266aede
akselhm <akselhm@stud.ntnu.no>
2019-11-05 11:35:39 +0100

    added quad4_shape_functon, N4 and computed xy = [x(ksi, eta), y(ksi, eta)]

--- examples_TMM4135/quads.py
+++ examples_TMM4135/quads.py
@@ -10,13 +10,13 @@ def quad4e(ex,ey,D,th,eq=None):
     """
     Compute the stiffness matrix for a four node membrane element.
 
-    :param list ex: element x coordinates [x1, x2, x3]
-    :param list ey: element y coordinates [y1, y2, y3]
+    :param list ex: element x coordinates [x1, x2, x3, x4]
+    :param list ey: element y coordinates [y1, y2, y3, x4]
     :param list D : 2D constitutive matrix
     :param list th: element thickness
     :param list eq: distributed loads, local directions [bx, by]
-    :return mat Ke: element stiffness matrix [6 x 6]
-    :return mat fe: consistent load vector [6 x 1] (if eq!=None)
+    :return mat Ke: element stiffness matrix [8 x 8]
+    :return mat fe: consistent load vector [8 x 1] (if eq!=None)
     """
 
     ex1 = ex[0:3]
@@ -28,6 +28,31 @@ def quad4e(ex,ey,D,th,eq=None):
     fe = np.matrix(np.zeros((8,1)))
 
     # TODO: fill out missing parts (or reformulate completely)
+    def quad4_shape_function(ksi, eta):
+        #assume ksi = [ksi1, ksi2, ksi3, ksi4], and same for eta
+        N4 = np.zeros(4)
+        N4[0] = 0.25 * (1 + ksi)*(1 + eta)
+        N4[1] = 0.25 * (1 - ksi)*(1 + eta)
+        N4[2] = 0.25 * (1 + ksi)*(1 - eta)
+        N4[3] = 0.25 * (1 - ksi)*(1 - eta)
+
+        return N4
+    
+    N4matrix = np.zeros((2,8))
+    # N4matrix =[[N4 , 0],
+    #            [0 , N4]]
+    N4matrix[0, :4] = N4
+    N4matrix[1, 4:] = N4
+
+    xyvector = np.zeros(8) 
+    # [x1, x2, ... , y3, y4]
+    xyvector[:4] = ex
+    xyvector[4:] = ey
+
+    xy = N4matrix * xyvector.T # vector [x(ksi, eta), y(ksi, eta)]
+
+    
+
 
     if eq is None:
         return Ke
