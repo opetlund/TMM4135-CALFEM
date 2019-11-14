52b8aa184176c2118ff4da3ec6a87f1b1871449c
akselhm <akselhm@stud.ntnu.no>
2019-11-14 14:48:58 +0100

    changed ksi to xsi

--- examples_TMM4135/quads_with_TODO.py
+++ examples_TMM4135/quads_with_TODO.py
@@ -41,17 +41,17 @@ def gauss_points(iRule):
     return gauss_position[idx], gauss_weight[idx]
 
 
-def quad4_shapefuncs(ksi, eta):
+def quad4_shapefuncs(xsi, eta):
     """
     Calculates shape functions evaluated at xi, eta
     """
     # ----- Shape functions -----
     # TODO: fill inn values of the  shape functions
     N4 = np.zeros(4)
-    N4[0] = 0.25 * (1 + ksi) * (1 + eta)
-    N4[1] = 0.25 * (1 - ksi) * (1 + eta)
-    N4[2] = 0.25 * (1 + ksi) * (1 - eta)
-    N4[3] = 0.25 * (1 - ksi) * (1 - eta)
+    N4[0] = 0.25 * (1 + xsi) * (1 + eta)
+    N4[1] = 0.25 * (1 - xsi) * (1 + eta)
+    N4[2] = 0.25 * (1 + xsi) * (1 - eta)
+    N4[3] = 0.25 * (1 - xsi) * (1 - eta)
     return N4
 
 def quad4_shapefuncs_grad_xsi(xsi, eta):
@@ -70,17 +70,17 @@ def quad4_shapefuncs_grad_xsi(xsi, eta):
     return Ndxi
 
 
-def quad4_shapefuncs_grad_eta(ksi, eta):
+def quad4_shapefuncs_grad_eta(xsi, eta):
     """
     Calculates derivatives of shape functions wrt. eta
     """
     # ----- Derivatives of shape functions with respect to eta -----
     # TODO: fill inn values of the  shape functions gradients with respect to xsi
     Ndeta = np.zeros(4)
-    Ndeta[0] = 0.25 * (1 + ksi)
-    Ndeta[1] = 0.25 * (1 - ksi)
-    Ndeta[2] = 0.25 * (-1 - ksi)
-    Ndeta[3] = 0.25 * (-1 + ksi)
+    Ndeta[0] = 0.25 * (1 + xsi)
+    Ndeta[1] = 0.25 * (1 - xsi)
+    Ndeta[2] = 0.25 * (-1 - xsi)
+    Ndeta[3] = 0.25 * (-1 + xsi)
     return Ndeta
 
 
