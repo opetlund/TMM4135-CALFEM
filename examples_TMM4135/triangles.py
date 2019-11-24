30314cfdc30f2d52f1a6c463fde90583bf50c3b9
opetlund <opetl@hotmail.com>
2019-11-24 16:07:14 +0100

    Morna, triangles.py helt ferdig.

--- examples_TMM4135/triangles.py
+++ examples_TMM4135/triangles.py
@@ -42,9 +42,12 @@ def tri3e(ex, ey, D, th, eq=None):
 
     cyclic_ijk = [0, 1, 2, 0, 1]      # Cyclic permutation of the nodes i,j,k
 
+    # Get partial derivatives of zeta / area coordinates
     zeta_px, zeta_py = zeta_partials_x_and_y(ex, ey)
 
+    # Initiate B to zeros
     B = np.mat(np.zeros((3, 6)))
+
     # Set columns of B
     for i in range(3):
         B[:, i * 2] = np.array([[zeta_px[i]], [0], [zeta_py[i]]])
@@ -55,6 +58,7 @@ def tri3e(ex, ey, D, th, eq=None):
     if eq is None:
         return Ke
     else:
+        # Distributed load on element
         fx = A * th * eq[0] / 3.0
         fy = A * th * eq[1] / 3.0
         fe = np.mat([[fx], [fy], [fx], [fy], [fx], [fy]])
@@ -75,14 +79,15 @@ def zeta_partials_x_and_y(ex, ey):
 
     A2 = np.linalg.det(tmp)  # Double of triangle area
 
-    cyclic_ijk = [0, 1, 2, 0, 1]      # Cyclic permutation of the nodes i,j,k
-
     zeta_px = np.zeros(3)           # Partial derivative with respect to x
     zeta_py = np.zeros(3)           # Partial derivative with respect to y
 
     for i in range(3):
+        # Cyclic permutation
         j = (i + 1) % 3
         k = (i + 2) % 3
+
+        # Set derivatives
         zeta_px[i] = (ey[j] - ey[k]) / A2
         zeta_py[i] = (ex[k] - ex[j]) / A2
 
@@ -103,14 +108,18 @@ def tri6_area(ex, ey):
 
 
 def tri6_shape_functions(zeta):
-    # Assumes zeta = [zeta1 zeta2 zeta3]
-    cyclic_ijk = [0, 1, 2, 0, 1]      # Cyclic permutation of the nodes i,j,k
 
+    # Interpolation polynomial vector
     N6 = np.zeros(6)
 
     for i in range(3):
+        # Set corner interpolation polynomial
         N6[i] = 2 * zeta[i] * (zeta[i] - 0.5)
+
+        # Cyclic permutation
         j = (i + 1) % 3
+
+        # Edge interpolation polynomial
         N6[i + 3] = 4 * zeta[i] * zeta[j]
 
     return N6
@@ -118,29 +127,38 @@ def tri6_shape_functions(zeta):
 
 def tri6_shape_function_partials_x_and_y(zeta, ex, ey):
 
+    # Get zeta partial derivatives
     zeta_px, zeta_py = zeta_partials_x_and_y(ex, ey)
 
+    # Initiate vectors for interpolation polynomial derivatives vectors
     N6_px = np.zeros(6)
     N6_py = np.zeros(6)
 
-    cyclic_ijk = [0, 1, 2, 0, 1]      # Cyclic permutation of the nodes i,j,k
-
     for i in range(3):
+        # Cyclic permutation
         j = (i + 1) % 3
+
+        # Set corner interpolation polynomial derivatives
         N6_px[i] = (4 * zeta[i] - 1) * zeta_px[i]
         N6_py[i] = (4 * zeta[i] - 1) * zeta_py[i]
+
+        # Set edge interpolation polynomial derivatives
         N6_px[i + 3] = 4 * (zeta[i] * zeta_px[j] + zeta[j] * zeta_px[i])
         N6_py[i + 3] = 4 * (zeta[i] * zeta_py[j] + zeta[j] * zeta_py[i])
+
     return N6_px, N6_py
 
 
 def tri6_Bmatrix(zeta, ex, ey):
 
+    # Get interpolation polynomial derivatives
     nx, ny = tri6_shape_function_partials_x_and_y(zeta, ex, ey)
 
+    # Initiate B matrix
     Bmatrix = np.matrix(np.zeros((3, 12)))
 
     for i in range(6):
+        # Set columns of B
         Bmatrix[:, i * 2] = np.array([[nx[i]], [0], [ny[i]]])
         Bmatrix[:, i * 2 + 1] = np.array([[0], [ny[i]], [nx[i]]])
 
@@ -153,32 +171,46 @@ def tri6_Kmatrix(ex, ey, D, th, eq=None):
     zetaInt = np.array([[0.5, 0.5, 0.0],
                         [0.0, 0.5, 0.5],
                         [0.5, 0.0, 0.5]])
+
     # Weights for numerical integration
     wInt = np.array([1.0/3.0, 1.0/3.0, 1.0/3.0])
 
+    # Area of element
     A = tri6_area(ex, ey)
 
+    # Initiate results
     Ke = np.matrix(np.zeros((12, 12)))
     fe = np.matrix(np.zeros((12, 1)))
 
     # Numerical integration with Gauss quadrature for triangle element
     for i in range(len(wInt)):
 
+        # current zeta values and weight
         zeta = zetaInt[:, i]
         weight = wInt[i]
 
+        # Get B matrix
         B = tri6_Bmatrix(zeta, ex, ey)
+
+        # Add to Ke
         Ke = Ke + weight * A * th * (B.T @ D @ B)
 
         if eq is not None:
+            # Distributed forces vector
             fvec = np.array([[eq[0]], [eq[1]]])
+
+            # interpolation polynomial vector
             N6 = tri6_shape_functions(zeta)
+
+            # Initiate N* matrix
             N2mat = np.zeros((2, 12))
 
             for j in range(6):
+                # Place interpolation polynomials in N*
                 N2mat[0, j * 2] = N6[j]
                 N2mat[1, j * 2 + 1] = N6[j]
 
+            # Add to fe
             fe += N2mat.T @ fvec * A * weight * th
 
     if eq is None:
@@ -189,98 +221,3 @@ def tri6_Kmatrix(ex, ey, D, th, eq=None):
 
 def tri6e(ex, ey, D, th, eq=None):
     return tri6_Kmatrix(ex, ey, D, th, eq)
-
-
-"""
-Version 2
-Not working
-"""
-
-# def tri6_shape_functions_partials_ksi_and_ny(zeta):
-#     ksi = zeta[0]
-#     ny = zeta[1]
-#     N_ksi = np.zeros(6)
-#     N_ny = np.zeros(6)
-#     N_ksi[0] = 4 * ksi - 1
-#     N_ny[1] = 4 * ny - 1
-#     N_ksi[2] = -3 + 2 * ny + 4 * ksi
-#     N_ny[2] = -3 + 2 * ksi + 4 * ny
-#     N_ksi[3] = 4 * ny
-#     N_ny[3] = 4 * ksi
-#     N_ksi[4] = -4 * ny
-#     N_ny[4] = 4 - 4 * ksi - 8 * ny
-#     N_ksi[5] = 4 - 8 * ksi - 4 * ny
-#     N_ny[5] = -4 * ksi
-#     return N_ksi, N_ny
-
-
-# def tri6_Jacobian(P, X):
-#     J = np.matmul(P, X)
-#     return J
-
-
-# def tri6_Bmatrix(J, P):
-#     N_x_y = np.matmul(np.linalg.inv(J), P)
-#     B = np.zeros((3, 12))
-#     for i in range(6):
-#         # B[:, i * 2] = np.array([[N_x_y[0, i]], [0], [N_x_y[1, i]]])
-#         # B[:, i * 2 + 1] = np.array([[0], [N_x_y[1, i]], [N_x_y[0, i]]])
-#         B[:, i * 2] = np.array([N_x_y[0, i], 0, N_x_y[1, i]])
-#         B[:, i * 2 + 1] = np.array([0, N_x_y[1, i], N_x_y[0, i]])
-
-#     return B
-
-
-# def tri6_Kmatrixold(ex, ey, D, th, eq=None):
-
-#     # zeta i values for numerical integration
-#     zetaInt = np.array([[0.5, 0.5, 0.0],
-#                         [0.0, 0.5, 0.5],
-#                         [0.5, 0.0, 0.5]])
-#     # Weights for numerical integration
-#     wInt = np.array([1.0/3.0, 1.0/3.0, 1.0/3.0])
-
-#     A = tri6_area(ex, ey)
-
-#     # Jacobian
-
-#     Ke = np.matrix(np.zeros((12, 12)))
-
-#     X = np.zeros((6, 2))
-#     X[:, 0] = ex
-#     X[:, 1] = ey
-#     P = np.zeros((2, 6))
-#     # Numerical integration with Gauss quadrature for triangle element
-#     for i in range(len(wInt)):
-#         N_ksi, N_ny = tri6_shape_functions_partials_ksi_and_ny(zetaInt[:, i])
-#         P[0, :] = N_ksi
-#         P[1, :] = N_ny
-#         J = tri6_Jacobian(P, X)
-#         print("P")
-#         print(P)
-#         print("J")
-#         print(J)
-#         B = tri6_Bmatrix(J, P)
-#         Ke = Ke + wInt[i] * B.T * D * B * np.linalg.det(J)
-
-#     if eq is None:
-#         return Ke
-#     else:
-#         fe = np.matrix(np.zeros((12, 1)))
-
-#         # TODO: fill out missing parts (or reformulate completely)
-
-#         return Ke, fe
-
-
-# def tri6_Jacobian_v1(zeta, ex, ey):
-
-#     nx, ny = tri6_shape_function_partials_x_and_y(zeta, ex, ey)
-#     J = np.zeros((2, 2))
-#     for i in range(6):
-#         J[0, 0] += nx[i] * ex[i]
-#         J[0, 1] += nx[i] * ey[i]
-#         J[1, 0] += ny[i] * ex[i]
-#         J[1, 1] += ny[i] * ey[i]
-
-#     return 0.5 * np.abs(np.linalg.det(J))
